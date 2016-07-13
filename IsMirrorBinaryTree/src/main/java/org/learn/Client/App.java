40c2c582a25883dc37806aea6751d9dd92f6932f
makeinjava <makeinjava@gmail.com>
2016-07-13 22:27:47 +0530

    Update package and refactor code

--- IsMirrorBinaryTree/src/main/java/org/learn/Client/App.java
+++ IsMirrorBinaryTree/src/main/java/org/learn/Client/App.java
@@ -1,34 +1,59 @@
 package org.learn.Client;
 
-import org.learn.PrepareTree.Node;
-import org.learn.PrepareTree.Tree;
 import org.learn.Question.IsMirrorTree;
+import org.learn.Question.Node;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-	public static void main( String[] args )
-    {		
-		int[] data1 = {50, 150, 25, 80, 125, 160};
-		int[] data2 = {150, 50, 160, 125,80,25};
+public class App {
+	public static void main(String[] args) {
+		// Binary Tree 1:
+		// root level 0
+		Node A = Node.createNode(70);
+		// Level 1
+		Node B = Node.createNode(50);
+		Node C = Node.createNode(55);
+		// Level 2
+		Node D = Node.createNode(25);
+		Node E = Node.createNode(80);
+		Node F = Node.createNode(37);
+		Node G = Node.createNode(45);
+
+		// connect Level 0 and 1
+		A.left = B;
+		A.right = C;
+		// connect level 1 and level 2
+		B.left = D;
+		B.right = E;
+		C.left = F;
+		C.right = G;
+
+		//Binary Tree 2:
 		
-    	Tree tree1 = new Tree();
-    	Node root1 = tree1.createNode(100);
-    	for(int i : data1 ) {
-    		tree1.insert(root1, i );
-    	}  
-    	Node root2 = tree1.createNode(100);
-    	for(int i : data2 ) {
-    		tree1.insert(root2, i );
-    	}  
-    	
-    	System.out.println("Binary Tree 1");
-    	tree1.printTree(root1);
-    	System.out.println("Binary Tree 2");
-    	tree1.printTree(root2);
-    	System.out.printf("Is Mirror Binary Tree :%b",IsMirrorTree.isMirrorTree(root1,root2));  	
-    }
+		// root level 0
+		Node P = Node.createNode(70);
+		// Level 1
+		Node Q = Node.createNode(55);
+		Node R = Node.createNode(50);
+		// Level 2
+		Node S = Node.createNode(45);
+		Node T = Node.createNode(37);
+		Node U = Node.createNode(80);
+		Node V = Node.createNode(25);
+
+		// connect Level 0 and 1
+		P.left = Q;
+		P.right = R;
+		// connect level 1 and level 2
+		Q.left = S;
+		Q.right = T;
+		R.left = U;
+		R.right = V;
+
+		System.out.println("Binary Tree 1 :");
+		IsMirrorTree.printTree(A);
+		System.out.println("Binary Tree 2 :");
+		IsMirrorTree.printTree(P);
+		
+		boolean isMirrorBinaryTrees = IsMirrorTree.isMirrorTree(A, P);
+		System.out.printf("Check Trees are mirror binary tree : %b", isMirrorBinaryTrees);
+	}
 }
--- IsMirrorBinaryTree/src/main/java/org/learn/Question/IsMirrorTree.java
+++ IsMirrorBinaryTree/src/main/java/org/learn/Question/IsMirrorTree.java
@@ -1,7 +1,8 @@
 
 package org.learn.Question;
 
-import org.learn.PrepareTree.Node;
+import java.util.LinkedList;
+import java.util.Queue;
 
 public class IsMirrorTree {
 	public static boolean isMirrorTree(Node tree, Node mirrorTree) {
@@ -21,5 +22,26 @@ public class IsMirrorTree {
 			return false;
 		
 		return true;
+	}
+	
+	public static void printTree(Node root) {
+		if (root == null) {
+			System.out.println("Tree is empty");
+			return ;
+		}
+		Queue<Node> queue = new LinkedList<Node>();
+		queue.offer(root);
+		while (!queue.isEmpty()) {
+			Node node = queue.poll();			
+			System.out.printf("%d ",node.data);		
+			if (node.left != null) {
+				queue.offer(node.left);
+			}
+			if (node.right != null) {
+				queue.offer(node.right);
+			}
+		}
+		System.out.println("");	
+		return;
 	}
 }
